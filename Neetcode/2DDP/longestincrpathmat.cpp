985993e4f33d4de0a131d2f9e9b75fb745d949ea
eric-zzhou <ericsuper@outlook.com>
2024-08-03 00:15:58 -0700

    Tried improving  solution for longest increasing path

--- Neetcode/2DDP/longestincrpathmat.cpp
+++ Neetcode/2DDP/longestincrpathmat.cpp
@@ -5,6 +5,33 @@
 #include <vector>
 using namespace std;
 
+// Slightly more optimized version of my solution, ig it's better to not save
+//  and pass lengths contrary to neetcode (NVM IT'S SAME SPEED AS MINE LOL)
+class Solution {
+ public:
+  int dfs(int i, int j, vector<vector<int>>& a, vector<vector<int>>& track) {
+    if (track[i][j] != -1) return track[i][j];
+    int m = 1;
+    if (i + 1 < a.size() && a[i + 1][j] > a[i][j])
+      m = max(m, 1 + dfs(i + 1, j, a, track));
+    if (j + 1 < a[0].size() && a[i][j + 1] > a[i][j])
+      m = max(m, 1 + dfs(i, j + 1, a, track));
+    if (i - 1 >= 0 && a[i - 1][j] > a[i][j])
+      m = max(m, 1 + dfs(i - 1, j, a, track));
+    if (j - 1 >= 0 && a[i][j - 1] > a[i][j])
+      m = max(m, 1 + dfs(i, j - 1, a, track));
+    return track[i][j] = m;
+  }
+  int longestIncreasingPath(vector<vector<int>>& a) {
+    int ret = 0;
+    vector<vector<int>> track(a.size(), vector<int>(a[0].size(), -1));
+    for (int i = 0; i < a.size(); i++) {
+      for (int j = 0; j < a[0].size(); j++) ret = max(ret, dfs(i, j, a, track));
+    }
+    return ret;
+  }
+};
+
 // MY SOLUTION WAS OPTIMAL !!!
 class MySolution {
  public:
