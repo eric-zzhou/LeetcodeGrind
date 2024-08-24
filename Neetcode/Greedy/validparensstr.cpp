53e97db1cc9379ae950178fe57a49bbd3ba86f75
eric-zzhou <ericsuper@outlook.com>
2024-08-24 05:15:06 -0400

    Finished valid parens string and all greedy

--- Neetcode/Greedy/validparensstr.cpp
+++ Neetcode/Greedy/validparensstr.cpp
@@ -7,22 +7,31 @@ using namespace std;
 class Solution {
  public:
   bool checkValidString(string s) {
-    int free = 0, track = 0;
-    for (int i = 0; i < s.size(); i++) {
-      if (s[i] == '*') {
-        free++;
-      } else if (s[i] == '(') {
-        track++;
-      } else {
-        track--;
+    // lmin tracks least number of left brackets, lmax tracks most number of
+    //  left brackets
+    int lmin = 0, lmax = 0;
+    for (char c : s) {
+      if (c == '(') {
+        lmin++;
+        lmax++;
+      } else if (c == ')') {
+        lmin--;
+        lmax--;
       }
-      if (track < 0) {
-        if (free == 0) {
-          return false;
-        }
-        free--;
+      // wildcard can be either left or right bracket
+      else {
+        lmin--;
+        lmax++;
       }
+
+      // more right brackets than left occurred, meaning invalid
+      if (lmax < 0) {
+        return false;
+      }
+      // lmin can't be negative
+      lmin = max(lmin, 0);
     }
-    return track - free <= 0;
+    // lmin == 0 means all left brackets can be matched
+    return lmin == 0;
   }
 };
