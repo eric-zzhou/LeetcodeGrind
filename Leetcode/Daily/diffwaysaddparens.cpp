c0eacce7df5718a9ce283bc6b564007e6d3631f1
eric-zzhou <ericsuper@outlook.com>
2024-09-19 02:51:18 -0400

    Finished daily problem

---
+++ Leetcode/Daily/diffwaysaddparens.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/different-ways-to-add-parentheses
+// 09/19/2024
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+class Solution {
+ public:
+  unordered_map<string, vector<int>> memo;
+  vector<int> diffWaysToCompute(string expression) {
+    if (memo.find(expression) != memo.end()) {
+      return memo[expression];
+    }
+    vector<int> ret;
+    char curr;
+    vector<int> lefts, rights;
+    for (int i = 0; i < expression.size(); i++) {
+      curr = expression[i];
+      if (curr == '+' || curr == '-' || curr == '*') {
+        lefts = diffWaysToCompute(expression.substr(0, i));
+        rights = diffWaysToCompute(expression.substr(i + 1));
+        for (int l : lefts) {
+          for (int r : rights) {
+            if (curr == '+') {
+              ret.push_back(l + r);
+            } else if (curr == '-') {
+              ret.push_back(l - r);
+            } else {
+              ret.push_back(l * r);
+            }
+          }
+        }
+      }
+    }
+    if (ret.empty()) {
+      ret.push_back(stoi(expression));
+    }
+    memo[expression] = ret;
+    return ret;
+  }
+};
