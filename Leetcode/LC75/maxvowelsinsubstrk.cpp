7b83e018bd1954f8c0f4c5c0e1b8b22776abb222
eric-zzhou <ericsuper@outlook.com>
2024-09-22 14:05:59 -0400

    finished max vowels in substr w len k

---
+++ Leetcode/LC75/maxvowelsinsubstrk.cpp
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+
+#include <../standard.h>
+using namespace std;
+
+class Solution {
+ public:
+  bool isVowel(char c) {
+    return (c == 'a') || (c == 'e') || (c == 'i') || (c == 'o') || (c == 'u');
+  }
+
+  int maxVowels(string s, int k) {
+    int ret = 0;
+    int curr = 0;
+    for (int i = 0; i < k; i++) {
+      if (isVowel(s[i])) {
+        curr++;
+      }
+    }
+    ret = curr;
+    for (int i = k; i <= s.size(); i++) {
+      if (isVowel(s[i - k])) {
+        curr--;
+      }
+      if (isVowel(s[i])) {
+        curr++;
+      }
+      ret = max(ret, curr);
+    }
+    return ret;
+  }
+};
---
+++ standard.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <algorithm>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
